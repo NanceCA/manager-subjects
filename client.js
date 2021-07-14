7d9842745394c640c6c7d2876b390a18c0aff081
Nance <48306129+NanceCA@users.noreply.github.com>
2021-07-14 14:57:49 -0700

    cambios a javascript

--- client.js
+++ client.js
@@ -1,7 +1,9 @@
 // "onclick"run function to change class property to display:block from display:none;
 // Get the modal
+
+
 var modalNPS = document.getElementById("modalNPS");
-var modalDS= document.getElementById("modalDS");
+var modalDS = document.getElementById("modalDS");
 
 // Get the button that opens the modal
 var botonNPS = document.getElementById("abreNPS");
@@ -12,22 +14,24 @@ var span = document.getElementsByClassName("cerrar-modal")[0];
 
 
 // When the user clicks on the button, open the modal 
-botonNPS.onclick = function() {
+botonNPS.onclick = function () {
+  console.log("Opened NPS modal")
   modalNPS.style.display = "block";
 }
-botonDS.onclick = function() {
-    modalDS.style.display = "block";
-  }
+botonDS.onclick = function () {
+  modalDS.style.display = "block";
+}
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
+  console.log("Closed NPS modal")
   modalNPS.style.display = "none";
 }
-span.onclick = function() {
-    modalDS.style.display = "none";
-  }
-  
+span.onclick = function () {
+  modalDS.style.display = "none";
+}
+
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
